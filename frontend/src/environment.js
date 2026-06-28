ccb62f49b72940ef8d7a79caa6d8959210d8cb12
akshatjain142 <akshat049660@gmail.com>
2026-06-28 18:44:14 +0530

    fix errors

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,7 +1,7 @@
 const IS_PROD=true;
 
 const server=IS_PROD?
-" https://meethubbackend-5rbl.onrender.com":
+"https://meethubbackend-5rbl.onrender.com":
 "http:/localhost:8000"
 
 export default server;
